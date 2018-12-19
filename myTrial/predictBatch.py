2c716433a0fcdfa641b88b8e36163ae3d69f6614
kono <konotorie@gmail.com>
2018-12-19 23:17:06 +0900

    finish until ch4.2

---
+++ myTrial/predictBatch.py
@@ -0,0 +1,42 @@
+from sigmoid import sigmoid
+from softmax import softmax
+import numpy as np
+import sys, os
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+import pickle
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+def init_network():
+    with open('sample_weight.pkl', 'rb') as f:
+        network = pickle.load(f)
+        return network
+
+def predict(network, x):
+    W1,W2,W3 = network['W1'],network['W2'],network['W3']
+    b1,b2,b3 = network['b1'],network['b2'],network['b3']
+
+    a1 = np.dot(x,W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1,W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2,W3) + b3
+    y = softmax(a3)
+
+    return y
+
+x,t = get_data()
+network = init_network()
+batch_size = 100 #纏めて処理する画像の数
+accuracy_cnt = 0
+
+for i in range(0,len(x),batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis = 1) #axisの数字nは最大インデックスを認識するn次元を表す。今回なら1次元=1行ごとに最大インデックスを取得
+    accuracy_cnt += np.sum(p==t[i:i+batch_size])
+
+print('accuracy:' + str(float(accuracy_cnt) / len(x)))
