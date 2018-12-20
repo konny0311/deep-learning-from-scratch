a4c58f8198b2f3acf1878fe5a9763a0dd3eda906
tatsuya-kono <tatsuya.kono@automagi.jp>
2018-12-20 12:12:58 +0900

    finish until ch4.3

---
+++ myTrial/crossEntropyError.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def cross_entropy_error(y,t):
+    """
+    出力結果が正解データと比較してどれだけ「悪いか」を数値化する。
+    正解インデックスをiとすると、-logy[i](>0)が出力される。0に近い方が正解に近い。
+    Parameters
+    ---------
+    y : np.ndarray
+        出力結果
+    t : np.ndarray
+        正解データ
+
+    """
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    delta = 1e-7 #オーバーフローを防ぐために微小値を足す
+    return -np.sum(t * np.log(y + delta)) / batch_size
+
+    # return -np.sum(t*np.log(y+delta))
