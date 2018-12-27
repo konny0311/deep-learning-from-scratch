533c1821ed7a06619ba8c04559c46d90e65fe88c
tatsuya-kono <tatsuya.kono@automagi.jp>
2018-12-27 16:09:01 +0900

    finish until ch7.5

--- myTrial/layer.py
+++ myTrial/layer.py
@@ -1,6 +1,8 @@
 from crossEntropyError import cross_entropy_error
 import numpy as np
 from softmax import softmax
+from common.util import img2col, col2im
+
 
 class MulLayer:
 
@@ -124,5 +126,60 @@ class SoftmaxWithLoss: # TODO: 付録Aに詳細説明
             dx = self.y.copy()
             dx[np.arange(batch_size), self.t] -= 1
             dx = dx / batch_size
+
+        return dx
+
+class Convolution:
+
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape #filter
+        N, C, H, W = x.shape
+        out_h = int(1+(H+2*self.pad-FH)/self.stride)
+        out_w = int(1+(W+2*self.pad-FW)/self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        #numpy.reshape()で-1を使った次元の長さは他の次元を考慮して自動的に決定される
+        #今回の場合だと片方の次元がFNに決まると残りの次元が自動的に決まる
+        col_W = self.W.reshape(FN, -1).T
+        out = np.dot(col, col_W) + self.b
+        out = out.reshape(N, out_h, out_w, -1).transpose(0,3,1,2) #transposeは配列順番の入れ替え
+        #今回の例では(N,H,W,C)0,1,2,3=>(N,C,H,W)0,3,1,2元の形に戻す
+
+        return out
+
+class Pooling:
+
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        N,C,H,W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+        out = np.max(col, axis=1)
+        out = out.reshape(N, out_h, out_w, C).transpose(0,3,1,2)
+
+        return out
+
+    def backward(self, dout):
+        dout = dout.transpose(0,2,3,1)
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,))
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
 
         return dx
